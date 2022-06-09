53aec992f29b71d15332add0ba55deb196ad0b65
Jonathan Bayer <linuxgurugamer@gmail.com>
2022-06-09 10:12:11 -0400

    Added code to NoTweakscale patch to remove Tweakscale module from parts  Thanks to forum user @m4ti140 for a patch for the new Apollo capsules from BDB

--- ComfortableLanding/AssemblyVersion.cs
+++ ComfortableLanding/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("2.0.2.0")]
- [assembly: AssemblyFileVersion("2.0.2.0")]
+ [assembly: AssemblyVersion("2.0.2.2")]
+ [assembly: AssemblyFileVersion("2.0.2.2")]
